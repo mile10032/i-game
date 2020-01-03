6acb5afdb62b60e32be869ac724164163ed20779
mile10032 <57488122+mile10032@users.noreply.github.com>
2020-01-04 03:49:24 +0900

    ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: "top#index" 
+  resources :top,only:[:chat]do
+    collection do
+      get "chat"
+    end
+  end
+  resources :users, only: [:edit, :update]
 end
